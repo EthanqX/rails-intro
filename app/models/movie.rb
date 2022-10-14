b411d2a042670fded3d4c472813da95733d467a5
Ethan Xu <qx1608@berkeley.edu>
2022-10-14 01:01:17 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ActiveRecord::Base
+
+  def self.all_ratings
+    Movie.pluck(:rating).uniq
+  end
+
+  def self.with_ratings(ratings)
+    if ratings.nil?
+      Movie.all
+    else
+      Movie.where(rating: ratings)
+    end
+  end
+
 end
